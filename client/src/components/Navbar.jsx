04d891a5a144f04c9a407da6b56e0dc4fbbf5aed
Oliver Kellgren <oliver-fotboll@live.se>
2021-05-19 15:15:13 +0200

    fixed navbar

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+import styles from "../css/Navbar.module.css";
+
+const Navbar = () => {
+	return (
+		<nav className={styles.navbar}>
+			<NavLink className={styles.link} exact to="/">
+				Aubameyang
+			</NavLink>
+			<NavLink className={styles.link} exact to="/about">
+				About
+			</NavLink>
+			<NavLink className={styles.link} exact to="/booking">
+				Booking
+			</NavLink>
+		</nav>
+	);
+};
+
+export default Navbar;
