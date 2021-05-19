9ab549e987a8b4c5edc172e4048d2930518df14c
Philip <philip_angelin@hotmail.com>
2021-05-19 11:45:37 +0200

    Fixed createUser route
    Final boilerplate commit

--- api/routes/usersRoutes.js
+++ api/routes/usersRoutes.js
@@ -2,6 +2,6 @@ const express = require("express");
 const usersController = require("../controllers/usersController");
 const router = express.Router();
 
-router.post("/users", usersController.createUser);
+router.post("", usersController.createUser);
 
 module.exports = router;
