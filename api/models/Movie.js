c20eb3e56bc119fe04414d735eaa79eb69778066
Philip <philip_angelin@hotmail.com>
2021-05-20 00:43:03 +0200

    Added timestamps

--- api/models/Movie.js
+++ api/models/Movie.js
@@ -1,19 +1,22 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const movieSchema = new Schema({
-	screenings: [{ type: Schema.Types.ObjectId, ref: "Screening" }],
-	title: { type: String, required: true },
-	genre: { type: String, required: true },
-	description: { type: String, required: true },
-	poster: { type: String, required: true },
-	length: { type: Number, required: true },
-	trailer: { type: String, required: true },
-	age: { type: Number, required: true, default: 18 },
-	language: { type: String, required: true },
-	subtitles: { type: Boolean, required: true, default: true },
-	director: { type: String, required: true },
-	cast: { type: Array, required: true },
-});
+const movieSchema = new Schema(
+	{
+		screenings: [{ type: Schema.Types.ObjectId, ref: "Screening" }],
+		title: { type: String, required: true },
+		genre: { type: String, required: true },
+		description: { type: String, required: true },
+		poster: { type: String, required: true },
+		length: { type: Number, required: true },
+		trailer: { type: String, required: true },
+		age: { type: Number, required: true, default: 18 },
+		language: { type: String, required: true },
+		subtitles: { type: Boolean, required: true, default: true },
+		director: { type: String, required: true },
+		cast: { type: Array, required: true },
+	},
+	{ timestamps: true }
+);
 
 module.exports = mongoose.model("Movie", movieSchema);
