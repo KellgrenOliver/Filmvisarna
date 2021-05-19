3b364d01aa586b185be58710af10f409a22d084c
Hitomi <hitomi.winberg@gmail.com>
2021-05-19 15:09:00 +0200

    Added reauired to  every properties

--- api/models/Seat.js
+++ api/models/Seat.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const seatSchema = new Schema ({
-  row:String,
-  number:Number,
-  type:{type:String, default:"adult"}
+  row: { type: String, required: true },
+  number: { type: Number, required: true },
+  type: {type:String, default:"adult", required: true}
 });
 
 const Seat = mongoose.model("Seat", seatSchema);
