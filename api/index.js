af74e8c35ea41eee25a426ab8b2cf65819900661
Philip <philip_angelin@hotmail.com>
2021-05-19 11:08:35 +0200

    Added Mongoose connection and workaround ENV json

--- api/index.js
+++ api/index.js
@@ -1,6 +1,12 @@
-require("dotenv").config();
 const express = require("express");
 const app = express();
-const { PORT } = process.env ?? 3001;
+const mongoose = require("mongoose");
+const { PORT, MONGODB_PASSWORD } = require("../env.json");
+
+mongoose
+	.connect(
+		`mongodb+srv://aubameyang:${MONGODB_PASSWORD}@cluster0.rvi3m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+	)
+	.then(() => console.log("Connected to MongoDB"));
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
