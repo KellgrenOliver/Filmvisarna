05c45e57d954ddce794480a1c9e63c79e96eb564
Axel Björnfot <axelbjornfot@gmail.com>
2021-05-19 14:51:41 +0200

    added bcrypt

--- api/models/User.js
+++ api/models/User.js
@@ -1,19 +1,31 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-
-const Encrypt = require("../Encrypt");
+const bcrypt = require("bcrypt");
 
 const userSchema = new Schema({
 	email: { type: String, required: true },
-	password: { type: String, required: true },
-	password: { type: Number, required: true },
+	password: { type: String, required: true, min: 6, max: 10 },
+	phone: { type: Number, required: true },
+	// role: { type: String, enum: "user", required: true },
 });
 
 userSchema.pre("save", async function (next) {
-	console.log("Inside mongoose middleware, this: ", this);
-	this.password = Encrypt.encrypt(this.password);
-	return next();
+	if (!this.isModified("password")) return next();
+	bcrypt.hash(this.password, 10, (err, passwordHash) => {
+		if (err) return next(err);
+		this.password = passwordHash;
+	});
 });
+
+userSchema.methods.comparePassword = function (password, cb) {
+	bcrypt.compare(password, this.password, (err, isMatch) => {
+		if (err) return cb(err);
+		else {
+			if (!isMatch) return cb(null, isMatch);
+			return cb(null, this);
+		}
+	});
+};
 
 const User = mongoose.model("User", userSchema);
 
