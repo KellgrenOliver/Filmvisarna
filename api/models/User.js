06b4022d50f0b04db47783855c9f8691ab4b6059
Axel Björnfot <axelbjornfot@gmail.com>
2021-05-20 09:30:34 +0200

    realation with bookings

--- api/models/User.js
+++ api/models/User.js
@@ -6,6 +6,7 @@ const userSchema = new Schema({
 	email: { type: String, required: true },
 	password: { type: String, required: true, min: 6, max: 10 },
 	phone: { type: Number, required: true },
+	bookings: [{ type: Schema.Types.ObjectId, ref: "Booking", required: true }],
 });
 
 userSchema.pre("save", async function (next) {
