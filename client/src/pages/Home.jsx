b6119d8004f4b183854e375dfe5c11ffeaebbeed
Oliver Kellgren <oliver-fotboll@live.se>
2021-05-20 12:06:57 +0200

    fixed responsive and styled cards

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -10,14 +10,30 @@ const Home = () => {
 		// eslint-disable-next-line
 	}, []);
 
+	// 	return (
+	// 		<div>
+	// 			{movies.map((movie, i) => (
+	// 				<div className={styles.cardWrapper} key={i}>
+	// 					<img className={styles.card} src={movie.poster} alt={movie.title} />
+	// 					<h2>{movie.title}</h2>
+	// 				</div>
+	// 			))}
+	// 		</div>
+	// 	);
+	// };
+
+	const renderMovies = () => {
+		return movies.map((movie) => (
+			<div key={movie.movie} className={styles.card}>
+				<img className={styles.img} src={movie.poster} alt={movie.title} />
+				{/* {movie.title} */}
+			</div>
+		));
+	};
+
 	return (
 		<div>
-			{movies.map((movie, i) => (
-				<div key={i}>
-					<img className={styles.img} src={movie.poster} alt={movie.title} />
-					<h2>{movie.title}</h2>
-				</div>
-			))}
+			<div className={styles.cardWrapper}>{movies && renderMovies()}</div>
 		</div>
 	);
 };
