0d8cb02a5991c093bfcf89d0b839d1045f727947
Mona Khorasani <mona.khorasani@medieinstitutet.se>
2021-05-20 22:51:47 +0200

    Created about page, style page, backgroudImage
    aboutPage is not responsive yet. background image works for desktop. Created assets map and app.module.css to fix background image.

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,18 @@
 import { BrowserRouter as Router, Route } from "react-router-dom";
-
+import styles from "./css/App.module.css";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import MovieProvider from "./contexts/MovieProvider";
+import AboutPage from "./pages/AboutPage";
 
 const App = () => {
 	return (
-		<div className="App">
+		<div className={styles.app}>
 			<Router>
 				<MovieProvider>
 					<Navbar />
 					<Route exact path="/" component={Home} />
+					<Route exact path="/about" component={AboutPage} />
 				</MovieProvider>
 			</Router>
 		</div>
