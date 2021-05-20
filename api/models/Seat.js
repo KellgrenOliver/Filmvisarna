34ec885495cd7b8a38b2025914808c6206d31b94
Hitomi <hitomi.winberg@gmail.com>
2021-05-20 10:11:21 +0200

    Changed seatSchema auditrium

--- api/models/Seat.js
+++ api/models/Seat.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const seatSchema = new Schema ({
-  row: { type: String, default:null },
-  type: { type:String, default:"adult", required: true },
-  reserved:{ type:Boolean, required: true, default: false },
-  _owners:[ { type:Schema.Types.ObjectId, ref:"Auditorium"} ]
+const seatSchema = new Schema({
+  row: { type: String, default: null },
+  id: { type: Number, required: true },
+  type: { type: String, default: "adult", required: true },
+  auditorium: { type: Schema.Types.ObjectId, ref: "Auditorium" },
 });
 
 const Seat = mongoose.model("Seat", seatSchema);
