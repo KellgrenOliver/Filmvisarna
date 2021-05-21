8a8d7d1d5419e300742ce1f4febb2dde069d8d8f
Philip <philip_angelin@hotmail.com>
2021-05-21 15:57:14 +0200

    Auditorium seeder

---
+++ api/utils/auditorium.js
@@ -0,0 +1,21 @@
+const errorLog = require("./errorLog");
+const auditoriums = require("../../auditoriums.json");
+const Auditorium = require("../models/Auditorium");
+
+async function auditoriumSeeder() {
+	try {
+		if (!auditoriums?.length) {
+			throw new Error(
+				`Expected auditoriums, got ${JSON.stringify(auditoriums)}`
+			);
+		}
+		await Auditorium.collection.drop();
+		await Auditorium.insertMany(auditoriums);
+	} catch (e) {
+		errorLog(e);
+	}
+}
+
+module.exports = {
+	auditoriumSeeder,
+};
