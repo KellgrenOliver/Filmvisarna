514f893ad28a8df41877c957015b12229a08f739
Philip <philip_angelin@hotmail.com>
2021-05-21 16:47:41 +0200

    Added user seeder to list

--- api/utils/seeder.js
+++ api/utils/seeder.js
@@ -2,8 +2,10 @@ const { seatsSeeder } = require("./seats");
 const { auditoriumSeeder } = require("./auditorium");
 const { movieSeeder } = require("./movie");
 const { screeningSeeder } = require("./screening");
+const { userSeeder } = require("./user");
 
 async function seeder() {
+	await userSeeder();
 	await movieSeeder();
 	await auditoriumSeeder();
 	await seatsSeeder();
