24cc7ae4bb36ca86197bcd548832ac6fed31f959
Philip <philip_angelin@hotmail.com>
2021-05-21 15:38:03 +0200

    Changed name

--- api/utils/seats.js
+++ api/utils/seats.js
@@ -3,7 +3,7 @@ const Seat = require("../models/Seat");
 const errorLog = require("./errorLog");
 const seats = require("../../seats.json");
 
-async function createStandardSeats() {
+async function seatsSeeder() {
 	try {
 		if (!seats?.length) {
 			throw new Error(`Expected seats, got ${JSON.stringify(seats)}`);
@@ -14,7 +14,7 @@ async function createStandardSeats() {
 		for (let i = 0; i < seats.length; i++) {
 			const { _id: auditorium } = await Auditorium.findOne().skip(i);
 			const result = seats[i].map((seat) => ({ ...seat, auditorium }));
-			Seat.insertMany(result);
+			await Seat.insertMany(result);
 		}
 	} catch (e) {
 		errorLog(e);
@@ -22,5 +22,5 @@ async function createStandardSeats() {
 }
 
 module.exports = {
-	createStandardSeats,
+	seatsSeeder,
 };
