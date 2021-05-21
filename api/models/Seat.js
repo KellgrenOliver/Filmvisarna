080c6832d8e35d9d0fbe97c863d386bfc1029a04
Philip <philip_angelin@hotmail.com>
2021-05-21 14:16:42 +0200

    Added seats creation seeder and fixed schema

--- api/models/Seat.js
+++ api/models/Seat.js
@@ -2,12 +2,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const seatSchema = new Schema({
-  row: { type: String, default: null },
-  id: { type: Number, required: true },
-  type: { type: String, default: "adult", required: true },
-  auditorium: { type: Schema.Types.ObjectId, ref: "Auditorium" },
+	row: { type: String, required: true },
+	id: { type: Number, required: true },
+	type: { type: String, default: "adult", required: true },
+	auditorium: { type: Schema.Types.ObjectId, ref: "Auditorium" },
 });
 
 const Seat = mongoose.model("Seat", seatSchema);
 
-module.exports = Seat
+module.exports = Seat;
