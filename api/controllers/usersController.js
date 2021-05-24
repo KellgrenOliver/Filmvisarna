f0dd5e648dc5d95f00091b1e701698967a6a42f2
Axel Björnfot <axelbjornfot@gmail.com>
2021-05-24 10:52:39 +0200

    fixed hash check

--- api/controllers/usersController.js
+++ api/controllers/usersController.js
@@ -17,8 +17,9 @@ const logout = (req, res) => {
 const login = async (req, res) => {
 	let userExists = await User.exists({ email: req.body.email });
 	if (userExists) {
-		let user = await User.findOne({ email: req.body.email }).exec();
-		if (user.password === req.body.password) {
+		let user = await User.findOne({ email: req.body.email });
+		const match = await bcrypt.compare(req.body.password, user.password);
+		if (match) {
 			req.session.user = user;
 			req.session.user.password = undefined;
 			user.password = undefined;
