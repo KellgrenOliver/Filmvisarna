4b60b79bd0132986f9d0567f88a65a034163e3e1
Philip <philip_angelin@hotmail.com>
2021-05-25 13:34:03 +0200

    Added remove booking functionality

--- api/controllers/bookingsController.js
+++ api/controllers/bookingsController.js
@@ -1,12 +1,11 @@
 const Auditorium = require("../models/Auditorium");
 const Booking = require("../models/Booking");
 const Movie = require("../models/Movie");
+const User = require("../models/User");
 const errorLog = require("../utils/errorLog");
 
 async function placeBooking(req, res) {
 	const { user } = req.session;
-	if (!user) return res.status(401).end();
-
 	const { seats, screening } = req.body;
 
 	try {
@@ -29,6 +28,24 @@ async function placeBooking(req, res) {
 	}
 }
 
+async function removeBooking(req, res) {
+	const { user } = req.session;
+	const { id } = req.params;
+
+	try {
+		const booking = await Booking.findOne({ _id: id, user });
+		if (!booking) {
+			return res.status(404).end();
+		}
+		await Booking.deleteOne(booking);
+		res.status(200).end();
+	} catch (e) {
+		errorLog(e);
+		res.status(500).end();
+	}
+}
+
 module.exports = {
 	placeBooking,
+	removeBooking,
 };
