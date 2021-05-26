e493756233c1f0154c1b867d674e77aeb528ddda
Mona Khorasani <mona.khorasani@medieinstitutet.se>
2021-05-26 10:15:25 +0200

    completing profilePage, edit personal information
    edit and update Btns show dependind on editMode

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,48 +1,125 @@
-import React from "react";
+import React, { useState, useContext, history, useEffect } from "react";
 import styles from "../css/ProfilePage.module.css";
-
+import { UserContext } from "../contexts/UserProvider";
 
 const ProfilePage = () => {
-	return (
-		<div className={styles.container}>
-			<h3>Welcome!</h3>
-			<div className={styles.flex}>
-				<div className={styles.info}>
-					<div>
-						<h6>Personal information</h6>
-					</div>
-					<hr></hr>
-					<div className={styles.flex}>
-						<span className={styles.infoDetail}>
-							Email: (comes from register page-data
-						</span>
-						<button className={styles.btn}>Edit</button>
-					</div>
-                    <div className={styles.flex}>
-						<span className={styles.infoDetail}>
-							Phone number: (comes from register page-data
-						</span>
-						<button className={styles.btn}>Edit</button>
-					</div>
-					<div className={styles.flex}>
-						<span className={styles.infoDetail}>
-							Password: (comes from register page-data
-						</span>
-						<button className={styles.btn}>Edit</button>
-					</div>
+	const [editMode, setEditMode] = useState(false);
+	const { whoami, user } = useContext(UserContext);
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [phoneNumber, setPhoneNumber] = useState("");
+
+	const onEdit = () => {
+		setEditMode(true);
+	};
+
+	const updateHandler = () => {
+		setEditMode(false);
+	};
+	// to be able to edit the phone number in input
+	useEffect(() => {
+		console.log(user);
+		if (user) {setPhoneNumber(user.phone);
+		setEmail(user.email);
+		setPassword(user.password);}
+	}, [user]);
+
+	useEffect(() => {
+		whoami();
+	}, []);
+
+	if (!user) {
+		return null;
+	} else {
+		let emailContent;
+		let passwordContent;
+		let phoneContent;
+		let buttonContent;
+
+		if (!editMode) {
+			emailContent = (
+				<span className={styles.infoDetail}>Email: {user.email}</span>
+			);
+			passwordContent = (
+				<span className={styles.infoDetail}>Password: {user.password}</span>
+			);
+			phoneContent = (
+				<span className={styles.infoDetail}>Phone number: {user.phone}</span>
+			);
+			buttonContent = (
+				<button className={styles.mainBtn} onClick={onEdit}>
+					Edit
+				</button>
+			);
+		} else {
+			emailContent = (
+				<div>
+					<label>Email address</label>
+					<input
+						type="text"
+						id="emailinput"
+						value={email}
+						onChange={(event) => setEmail(event.target.value)}
+					/>
 				</div>
-				<div className={styles.info}>
-					<h6>Last bookings</h6>
-					<hr />
-					<div className={styles.flex}>
-						<span>Bookings show here</span>
-						<button className={styles.btn}>Delete</button>
+			);
+			passwordContent = (
+				<div>
+					<label >Password</label>
+					<input
+						type="text"
+						id="passwordinput"
+						value={password}
+						onChange={(event) => setPassword(event.target.value)}
+					/>
+				</div>
+			);
+			phoneContent = (
+				<div>
+					<label>Phone Number</label>
+					<input
+						type="text"
+						id="numberinput"
+						value={phoneNumber}
+						onChange={(event) => setPhoneNumber(event.target.value)}
+					/>
+				</div>
+			);
+			buttonContent = (
+				<button className={styles.mainBtn} onClick={updateHandler}>
+					Update changes
+				</button>
+			);
+		}
+
+		return (
+			<div className={styles.container}>
+				<h3>Welcome!</h3>
+				<div className={styles.flex}>
+					<div className={styles.info}>
+						<div>
+							<h6>Personal information</h6>
+						</div>
+						<hr></hr>
+						<div>{emailContent}</div>
+						<div>{passwordContent}</div>
+						<div>
+							{phoneContent}
+							<div className={styles.flex}>{buttonContent}</div>
+						</div>
+					</div>
+					<div className={styles.info}>
+						<h6>Last bookings</h6>
+						<hr />
+						<div className={styles.flex}>
+							<span>Bookings show here</span>
+							<button className={styles.btn}>Delete</button>
+						</div>
 					</div>
 				</div>
 			</div>
-			<button className={styles.mainBtn}>Save changes</button>
-		</div>
-	);
+		);
+	}
 };
 
 export default ProfilePage;
