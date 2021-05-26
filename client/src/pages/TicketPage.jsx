b054345cad6a5dc15f95270af19d9811c5a4fa4f
Oliver Kellgren <oliver-fotboll@live.se>
2021-05-26 12:09:41 +0200

    Styled ticket page

--- client/src/pages/TicketPage.jsx
+++ client/src/pages/TicketPage.jsx
@@ -1,5 +1,6 @@
 import { MovieContext } from "../contexts/MoviesProvider";
 import { useContext } from "react";
+import NumericInput from "react-numeric-input";
 import React from "react";
 import styles from "../css/TicketPage.module.css";
 
@@ -15,28 +16,58 @@ const TicketPage = (props) => {
 	return (
 		<div className={styles.ticketPage}>
 			<div className={styles.container}>
-				<h4>Salong</h4>
-				<h4 className={styles.title}>{movie.title}</h4>
-				<span>Datum, tid</span>
-				<br />
-				<span>Åldersgräns, språk, text</span>
-				<h4>1. Välj stolar</h4>
-				<h4 className={styles.bioduk}>B I O D U K</h4>
-				<h4>Platser</h4>
+				<div className={styles.titleContainer}>
+					<h5>Salon</h5>
+					<h5 className={styles.title}>{movie.title}</h5>
+					<h5>Date, Time</h5>
+					<h5>Age limit, Language, Subtitle</h5>
+				</div>
+				<h5>1. Choose seats</h5>
+				<h5 className={styles.bioduk}>S C R E E N</h5>
+				<h5>Seats</h5>
 				<div className={styles.platserContainer}>
 					<div className={styles.tPlatser} />
-					<span className={styles.platserTitle}>Tillgängliga platser</span>
+					<span className={styles.platserTitle}>Available seats</span>
 					<div className={styles.oPlatser} />
-					<span className={styles.platserTitle}>Otillgängliga platser</span>
+					<span className={styles.platserTitle}>Unavailable seats</span>
 					<div className={styles.dStolval} />
-					<span className={styles.platserTitle}>Ditt stolval</span>
+					<span className={styles.platserTitle}>Your seat choice</span>
 				</div>
-				<h4>Välj antal biljetter</h4>
-				<span>Vuxen</span>
-				<br />
-				<span>Pensionär</span>
-				<br />
-				<span>Barn</span>
+				<h5>Select tickets</h5>
+				<div className={styles.numberContainer}>
+					<span className={styles.ticketTitle}>Adult </span>
+					<NumericInput
+						className={styles.numberInput}
+						min={0}
+						max={46}
+						value={0}
+					/>
+					<span className={styles.ticketTitle}>Pensioner </span>
+					<NumericInput
+						className={styles.numberInput}
+						min={0}
+						max={46}
+						value={0}
+					/>
+					<span className={styles.ticketTitle}>Child </span>
+					<NumericInput
+						className={styles.numberInput}
+						min={0}
+						max={46}
+						value={0}
+					/>
+				</div>
+
+				<div className={styles.payContainer}>
+					<img className={styles.img} src={movie.poster} alt={movie.title} />
+					<div className={styles.pay}>
+						<h6>{movie.title}</h6>
+						<h6>Time</h6>
+						<h6>Tickets</h6>
+						<h6>Price:</h6>
+						<button className={styles.button}>Book</button>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
