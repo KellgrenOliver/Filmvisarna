9288083d7ff0f0ea0a49173449161538d3cc64a0
Philip <philip_angelin@hotmail.com>
2021-05-27 12:56:18 +0200

    Refactored validateBody using lodash

--- api/utils/validation.js
+++ api/utils/validation.js
@@ -1,22 +1,11 @@
+const _ = require("lodash");
+
 // Validate whether the req.body keys matches the keys array
 function validateBody(body, keys) {
 	if (body == null || keys == null) {
 		return false;
 	}
-
-	body = Object.keys(body).sort();
-	keys = keys.sort();
-
-	if (body.length !== keys.length) {
-		console.log("did not match");
-		return false;
-	}
-
-	for (let i = 0; i < body.length; ++i) {
-		if (body[i] !== keys[i]) return false;
-	}
-
-	return true;
+	return _.difference(keys, Object.keys(body)).length === 0;
 }
 
 module.exports = {
