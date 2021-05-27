86002e1b7de1dd6abe0a4f7e1a758a4e0e07b65e
Hitomi <hitomi.winberg@gmail.com>
2021-05-27 10:22:39 +0200

    Joined search function and filter function

--- api/controllers/moviesController.js
+++ api/controllers/moviesController.js
@@ -3,47 +3,16 @@ const Screening = require("../models/Screening");
 
 const getAllMovies = async (req, res) => {
   try {
-    if (Object.keys(req.query).length === 0) {
-      let movies = await Movie.find().exec();
-      res.json(movies);
-      return;
-    }
-    let querySearch = new RegExp(`${req.query.search ?? ""}\\w*`, "gi");
-    let query = Movie.find({
-      $or: [{
-        title: querySearch
-      }, {
-        language: querySearch
-      }, {
-        genres: querySearch
-      }, {
-        directors: querySearch
-      }, {
-        stars: querySearch
-      }]
-    });
-    let movies;
-    movies = await query.exec();
-    if (movies.length === 0) {
-      res.status(404).json({
-        error: "The movies doesn't match"
-      })
-      return
-    }
-    res.json(movies)
-  } catch (err) {
-    res.status(400).send(err)
+    let movies = await Movie.find().exec();
+    res.json(movies);
+  } catch (error) {
+    console.log(error);
   }
 };
 
-// pris, datum, längd på filmen, åldersgräns, genre, skådespelare, regissör och språk.
-
-const getMoviesByFilter = async (req, res) => {
+const getMoviesByFilterAndSearch = async (req, res) => {
   try {
-    let queryPriceMin = req.query.priceMin ?? 0;
-    let queryPriceMax = req.query.priceMax ?? Infinity;
-    let startDate = req.query.startDate ?? new Date("0000-01-01");
-    let endDate = req.query.endDate ?? new Date("9999-12-31");
+    let querySearch = new RegExp(`${req.query.search ?? ""}\\w*`, "gi");
     let queryLengthMin = req.query.lengthMin ?? 0;
     let queryLengthMax = req.query.lengthMax ?? Infinity;
     let queryLanguage = new RegExp(`^${req.query.language ?? ""}\\w*`, 'gi');
@@ -52,18 +21,7 @@ const getMoviesByFilter = async (req, res) => {
     let queryStar = new RegExp(`^${req.query.star ?? ""}\\w*`, 'gi');
     let queryRating = new RegExp(`^${req.query.rating ?? ""}\\w*`, 'gi');
 
-    let movies = await Screening.find({
-      price: {
-        $gte: queryPriceMin,
-        $lte: queryPriceMax
-      },
-      time: {
-        $gte: new Date(startDate),
-        $lte: new Date(endDate)
-      }
-    }).select("movie").populate({
-      path: "movie",
-      match: {
+    let movies = await Movie.find({
         language: queryLanguage,
         genres: queryGenre,
         directors: queryDirector,
@@ -72,8 +30,19 @@ const getMoviesByFilter = async (req, res) => {
         length: {
           $gte: queryLengthMin,
           $lte: queryLengthMax
-        }
-      }
+        },       
+          $or: [{
+            title: querySearch
+          }, {
+            language: querySearch
+          }, {
+            genres: querySearch
+          }, {
+            directors: querySearch
+          }, {
+            stars: querySearch
+          }]       
+
     }).exec()
 
     if (movies.length === 0) {
@@ -87,6 +56,7 @@ const getMoviesByFilter = async (req, res) => {
     res.status(400).send(err)
   }
 }
+
 
 const getMovieById = (req, res) => {
   Movie.findById(req.params.movieId).exec((err, movie) => {
