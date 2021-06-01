0ec7b3a11eefb3445d81a8d50826543b53174b4b
Axel Björnfot <axelbjornfot@gmail.com>
2021-06-01 13:16:18 +0200

    render login btn when logged out

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,23 +4,22 @@ import { useContext, useEffect, useState } from "react";
 import styles from "../css/Navbar.module.css";
 
 const Navbar = () => {
-	const { logout, whoami, user, loggedIn } = useContext(UserContext);
+	const { logout, whoami, loggedIn } = useContext(UserContext);
 
 	const history = useHistory();
 
 	useEffect(() => {
 		whoami();
-	}, []);
+	}, [loggedIn]);
 
 	const handleSubmit = async () => {
 		let result = await logout();
 		console.log(result);
 		if (result.success) {
 			history.push("/");
-			console.log(user);
 		}
 	};
-	console.log(loggedIn);
+
 	return (
 		<nav className={styles.navbar}>
 			<NavLink className={styles.link} exact to="/">
