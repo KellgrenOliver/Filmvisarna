e1c76e3766c45a1d0a553bafc936054a8e4d53ac
Axel Björnfot <axelbjornfot@gmail.com>
2021-06-02 14:50:24 +0200

    FIxed bug when created user

--- client/src/contexts/UserProvider.jsx
+++ client/src/contexts/UserProvider.jsx
@@ -4,7 +4,7 @@ export const UserContext = createContext();
 
 const UserProvider = (props) => {
 	const [user, setUser] = useState(null);
-	const [ message, setMessage ] = useState(null);
+	const [message, setMessage] = useState(null);
 
 	const whoami = async () => {
 		let user = await fetch("/api/v1/users/whoami");
@@ -41,21 +41,22 @@ const UserProvider = (props) => {
 			body: JSON.stringify(userToCreate),
 		});
 		result = await result.json(userToCreate);
+		setUser(userToCreate);
 		return result;
 	};
 
 	// to save the changes which are coming from profile page
 
-	const updateUserInfo = async (userToUpdate) =>{
+	const updateUserInfo = async (userToUpdate) => {
 		// console.log(userToUpdate);
 		let result = await fetch(`api/v1/users/${user._id}`, {
 			method: "PUT",
 			headers: {
-				"content-type": "application/json"
+				"content-type": "application/json",
 			},
-			body: JSON.stringify(userToUpdate)
+			body: JSON.stringify(userToUpdate),
 		});
-	// to get the updated info from backend 
+		// to get the updated info from backend
 		result = await result.json(userToUpdate);
 		setMessage(result.success);
 		const updatedUser = result.updatedUser;
