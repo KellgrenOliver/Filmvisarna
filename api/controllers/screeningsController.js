9b29d39161d58cdb81f003012450c859faea9e0b
Philip <philip_angelin@hotmail.com>
2021-06-03 15:19:59 +0200

    Last refactoring

--- api/controllers/screeningsController.js
+++ api/controllers/screeningsController.js
@@ -37,15 +37,17 @@ async function getScreeningById(req, res) {
 }
 
 async function getScreeningsFromMovie(req, res) {
-	const { movie } = req.params;
 	try {
+		const { movie } = req.params;
+
 		if (Object.keys(req.query).length === 0) {
 			let screening = await Screening.find({ movie }).populate([
 				"movie",
 				"auditorium",
 			]);
+
 			if (screening.length === 0) {
-				return res.status(404).json({ error: "Not found " });
+				return res.status(404).json({ error: "No results were found." });
 			}
 
 			return res.status(200).json(screening);
