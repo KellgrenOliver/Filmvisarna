938c2c85166aabb93d62b8b5b25cb8336d36643a
Hitomi <hitomi.winberg@gmail.com>
2021-06-05 21:48:03 +0200

    All functions to get items moved to FilterGroup

--- client/src/components/FilterGroup.jsx
+++ client/src/components/FilterGroup.jsx
@@ -1,21 +1,92 @@
+import { useContext, useEffect, useState } from "react";
+import { MovieContext } from "../contexts/MoviesProvider";
 import Search from "./Search";
 import Length from "./Length";
-import Language from "./Language";
-import Genre from "./Genre";
-import Director from "./Director";
-import Star from "./Star";
-import Rating from "./Rating";
+import Items from "./Items";
 import styles from "../css/FilterGroup.module.css";
+
 const FilterGroup = () => {
+	const {
+		setLengthMin,
+		setLengthMax,
+		setLanguage,
+		setGenre,
+		setDirector,
+		setStar,
+		setRating,
+		movies,
+	} = useContext(MovieContext);
+	const [languageItems, setLanguageItems] = useState("");
+	const [genreItems, setGenreItems] = useState("");
+	const [directorItems, setDirectorItems] = useState("");
+	const [starItems, setStarItems] = useState("");
+	const [ratingItems, setRatingItems] = useState("");
+
+	useEffect(() => {
+		if (movies) {
+			getLanguageItems();
+			getGenreItems();
+			getDirectorItems();
+			getStarItems();
+			getRatingItems();
+		}
+		return () => {
+			setLanguage("");
+			setGenre("");
+			setDirector("");
+			setStar("");
+			setRating("");
+			setLengthMin(`?lengthMin=` + 0);
+			setLengthMax(`&lengthMax=` + Infinity);
+		};
+	}, [movies]);
+
+	const getLanguageItems = () => {
+		let values = movies.map((movie) => movie.language);
+		setLanguageItems([...new Set(values)]);
+	};
+
+	const getGenreItems = () => {
+		let values = movies.map((movie) => movie.genres);
+		let result = [];
+		values.forEach((value) => {
+			result = result.concat(value);
+		});
+		setGenreItems([...new Set(result)]);
+	};
+
+	const getDirectorItems = () => {
+		let values = movies.map((movie) => movie.directors);
+		let result = [];
+		values.forEach((value) => {
+			result = result.concat(value);
+		});
+		setDirectorItems([...new Set(result)]);
+	};
+
+	const getStarItems = () => {
+		let values = movies.map((movie) => movie.stars);
+		let result = [];
+		values.forEach((value) => {
+			result = result.concat(value);
+		});
+		setStarItems([...new Set(result)]);
+	};
+
+	const getRatingItems = () => {
+		let value = movies.map((movie) => movie.rating);
+		setRatingItems([...new Set(value)]);
+	};
+
 	return (
 		<div className={styles.groupContainer}>
 			<div className={styles.filterContainer}>
-				<Length />
-				<Language />
-				<Genre />
-				<Director />
-				<Star />
-				<Rating />
+				<Length setLengthMin={setLengthMin} setLengthMax={setLengthMax} />
+				<Items setVal={setLanguage} name={"language"} items={languageItems} />
+				<Items setVal={setGenre} name={"genres"} items={genreItems} />
+				<Items setVal={setDirector} name={"directors"} items={directorItems} />
+				<Items setVal={setStar} name={"stars"} items={starItems} />
+				<Items setVal={setRating} name={"rating"} items={ratingItems} />
 			</div>
 			<div className={styles.searchContainer}>
 				<Search />
--- client/src/components/Length.jsx
+++ client/src/components/Length.jsx
@@ -2,29 +2,21 @@ import { useContext, useEffect } from "react";
 import { MovieContext } from "../contexts/MoviesProvider";
 import styles from "../css/FilterGroup.module.css";
 
-const Length = () => {
-	const { setLengthMin, setLengthMax } =
-		useContext(MovieContext);
-
-	useEffect(() => {
-		return () => {
-			setLengthMin(`?lengthMin=` + 0);
-			setLengthMax(`&lengthMax=` + Infinity);
-		};
-	}, []);
-  
+const Length = (props) => {
+ 
 	const handleLength = (e) => {
 		if (e.target.value === "0") {
-			setLengthMin(`?lengthMin=` + 0);
-			setLengthMax(`&lengthMax=` + Infinity);
+			props.setLengthMin(`?lengthMin=` + 0);
+			props.setLengthMax(`&lengthMax=` + Infinity);
 		} else if (e.target.value === "1") {
-			setLengthMin(`?lengthMin=` + 0);
-			setLengthMax(`&lengthMax=` + 99);
+			props.setLengthMin(`?lengthMin=` + 0);
+			props.setLengthMax(`&lengthMax=` + 99);
 		} else if (e.target.value === "2") {
-			setLengthMin(`?lengthMin=` + 100);
-			setLengthMax(`&lengthMax=` + 199);
+			props.setLengthMin(`?lengthMin=` + 100);
+			props.setLengthMax(`&lengthMax=` + 199);
 		} else if (e.target.value === "3") {
-			setLengthMin(`?lengthMin=` + 200);
+			props.setLengthMin(`?lengthMin=` + 200);
+      props.setLengthMax("");
 		}
 	};
 
@@ -32,7 +24,7 @@ const Length = () => {
 		return (
 			<div className={styles.customSelect}>
 				<select className={styles.select} onChange={handleLength}>
-					<option className={styles.option} value="0">length:</option>
+					<option className={styles.option} value="0">Length:</option>
 					<option className={styles.option} value="1">0-99 min</option>
 					<option className={styles.option} value="2">100-199 min</option>
 					<option className={styles.option} value="3">200 min</option>
