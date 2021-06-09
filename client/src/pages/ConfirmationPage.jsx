8a7a66662ed2e04b2007649ffb52b25bf4a01efe
Oliver Kellgren <oliver-fotboll@live.se>
2021-06-09 15:00:09 +0200

    started style tickets

--- client/src/pages/ConfirmationPage.jsx
+++ client/src/pages/ConfirmationPage.jsx
@@ -20,7 +20,7 @@ const ConfirmationPage = (props) => {
 		return <h1 className={styles.header}>Loading...</h1>;
 	}
 
-	console.log(booking.movie.title);
+	console.log(booking.movie);
 
 	const content = () => (
 		<div className={styles.confirmationPage}>
@@ -30,25 +30,32 @@ const ConfirmationPage = (props) => {
 				</Link>
 				<h4>Thanks for your order, {user.email}!</h4>
 				{/* <h5>Salon: {screening.auditorium.id}</h5> */}
-				<h5>{booking.movie.title}</h5>
 				<h5>
 					{booking.seats.map((seat) => (
 						<div className={styles.ticket}>
+							<h5>{booking.movie.title}</h5>
 							Row: {seat.row}
 							Seat: {seat.id}
 							Price: {seat.price}
+							<h5>{dayjs(booking.screening.time).format("MMMM Do HH:mm")}</h5>
+							<img
+								className={styles.img}
+								src={booking.movie.poster}
+								alt={booking.movie.title}
+							/>
+							<img
+								className={styles.qrImg}
+								src={
+									"https://media.nbcbayarea.com/2020/10/qr-code-huge.png?resize=906,1024"
+								}
+								alt="QR Code"
+							/>
 						</div>
 					))}
 				</h5>
-				<h5>{dayjs(booking.screening.time).format("MMMM Do HH:mm")}</h5>
 				<h6>
 					Total: {getTicketsPrice(booking.seats, booking.screening.price)} SEK
 				</h6>
-				<img
-					className={styles.img}
-					src={booking.movie.poster}
-					alt={booking.movie.title}
-				/>
 			</div>
 		</div>
 	);
