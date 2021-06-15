b338c1b5cd1b7b7b6071bdc3996120a8ea0f17ad
Philip <philip_angelin@hotmail.com>
2021-06-15 10:33:59 +0200

    Created AuthRoute component that redirects if gust

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,14 +1,31 @@
 import { MovieContext } from "../contexts/MoviesProvider";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import FilterGroup from "../components/FilterGroup";
 import styles from "../css/HomePage.module.css";
+import Login from "../components/Login";
+import CreateUser from "../components/CreateUser";
+import { UserContext } from "../contexts/UserProvider";
 
-const Home = () => {
+const Home = ({ location, history }) => {
 	const { movies, searchedMovies, message } = useContext(MovieContext);
 	useEffect(() => {
 		renderMovies();
 	}, [searchedMovies, message]);
+
+	const { loggedIn } = useContext(UserContext);
+
+	const [loginOpen, setLoginOpen] = useState(() => {
+		if (location.state?.openModal) {
+			return true;
+		}
+		return false;
+	});
+	const [registerOpen, setRegisterOpen] = useState(false);
+
+	useEffect(() => {
+		history.push("/", { openModal: false });
+	}, [location.state?.openModal]);
 
 	const renderMovies = () => {
 		if (searchedMovies) {
@@ -25,15 +42,25 @@ const Home = () => {
 			));
 		}
 	};
-  
-  const renderMessage=()=>{
-    return <h3>{message}</h3>
-  }
+
+	const renderMessage = () => {
+		return <h3>{message}</h3>;
+	};
 
 	return (
 		<div className={styles.container}>
 			<FilterGroup />
-      <div className={styles.message}>{message && renderMessage()}</div>
+			<Login
+				onClose={() => setLoginOpen(false)}
+				onHandleClick={() => setRegisterOpen(true)}
+				show={!loggedIn && loginOpen}
+			/>
+			<CreateUser
+				onClose={() => setRegisterOpen(false)}
+				onOpen={() => setLoginOpen(true)}
+				showRegister={!loggedIn && registerOpen}
+			/>
+			<div className={styles.message}>{message && renderMessage()}</div>
 			<div className={styles.cardWrapper}>{movies && renderMovies()}</div>
 		</div>
 	);
